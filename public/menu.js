3567455fa5232c6160d07db12b96811861cbfe4d
Leon Bentrup <leon@leonadi.de>
2019-05-21 20:52:31 +0200

    access token

---
+++ public/menu.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function init() {
+    var nav = document.querySelector('nav')
+    var toggle = document.querySelector('.nav-toggle')
+
+    function toggleMenu() {
+        if (nav.classList.contains('open')) {
+            nav.classList.remove('open')
+        } else {
+            nav.classList.add('open')
+        }
+    }
+
+    function hideMenu(ev) {
+        var target = ev.target;
+        if (target.closest('nav') || target.closest('.nav-toggle')) return;
+        if (nav.classList.contains('open')) {
+            nav.classList.remove('open')
+            ev.preventDefault();
+        }
+    }
+
+    toggle.addEventListener('click', toggleMenu)
+    document.addEventListener('click', hideMenu)
+})()
